9094c02bace6d4c813ecc5d49a422a97467cae62
Nathaniel Adams <NathanielBAdams@gmail.com>
2020-09-26 08:28:37 -0400

    testing cookies

--- ninja-incognito/src/bg/background.js
+++ ninja-incognito/src/bg/background.js
@@ -1,31 +1,40 @@
-let newTabURL;
-chrome.windows.getAll({}, (windows) => {
-	windows.forEach((window) => {
-		if (window.focused === true) {
-			chrome.tabs.query({ windowId: window.id }, (tabs) => {
-				tabs.forEach((tab) => {
-					if (tab.active) {
-						newTabURL = tab.url;
-					}
-				});
-			});
-		}
-	});
+chrome.runtime.onStartup.addListener(() => {
+  chrome.storage.sync.set({ color: '#3aa757' }, function() {
+    console.log('The color is green.');
+  });
 });
 
-const button = document.querySelector('.ninja');
+chrome.cookies.getAll({}, (cookies) => {
+  console.log(cookies);
+});
+let newTabURL;
+chrome.windows.getAll({}, (windows) => {
+  windows.forEach((window) => {
+    if (window.focused === true) {
+      chrome.tabs.query({ windowId: window.id }, (tabs) => {
+        tabs.forEach((tab) => {
+          if (tab.active) {
+            newTabURL = tab.url;
+          }
+        });
+      });
+    }
+  });
+});
+
+const ninja = document.querySelector('.ninja');
 const info = document.querySelector('.info');
 const mainPopup = document.querySelector('#mainPopup');
 
-button.addEventListener('click', () => {
-	setTimeout(() => incognitoWindow(), 1000);
-	// change classes,
-	info.innerHTML = 'You have gone ninja.';
-	button.classList.add('img-ninja-mode');
-	// mainPopup.style.backgroundColor = '#292a2d';
-	mainPopup.classList.add('ninja-mode');
+ninja.addEventListener('click', () => {
+  setTimeout(() => incognitoWindow(), 1000);
+  // change classes,
+  info.innerHTML = 'You have gone ninja.';
+  button.classList.add('img-ninja-mode');
+  // mainPopup.style.backgroundColor = '#292a2d';
+  mainPopup.classList.add('ninja-mode');
 });
 
 function incognitoWindow() {
-	chrome.windows.create({ url: newTabURL, incognito: true });
+  chrome.windows.create({ url: newTabURL, incognito: true });
 }
